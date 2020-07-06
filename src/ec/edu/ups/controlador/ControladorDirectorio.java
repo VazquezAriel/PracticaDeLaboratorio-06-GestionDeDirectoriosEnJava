638d20ef742b0093d023e808e1527c89e9489da8
ariel <ariel@LAPTOP-MQ6B7FKQ.local>
2020-07-06 00:04:06 -0500

    Finalizacion de la practica(Correccion de errores)

--- src/ec/edu/ups/controlador/ControladorDirectorio.java
+++ src/ec/edu/ups/controlador/ControladorDirectorio.java
@@ -6,80 +6,164 @@
 package ec.edu.ups.controlador;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import javax.swing.DefaultListModel;
 
 /**
  *
  * @author ariel
  */
 public class ControladorDirectorio {
-    
+
     private String ruta;
     private File archivo;
-    private File[] archivos;
+    private DefaultListModel archivos;
 
     public ControladorDirectorio(String ruta) {
         this.ruta = ruta;
-    }
-
-    public String getRuta() {
-        return ruta;
-    }
-
-    public void setRuta(String ruta) {
-        this.ruta = ruta;
+        this.archivo = new File(this.ruta);
     }
 
     public File getArchivo() {
-        return archivo;
+        if (archivo.exists()) {
+            return archivo;
+        } else {
+            return null;
+        }
     }
 
     public void setArchivo(File archivo) {
         this.archivo = archivo;
     }
 
-    public File[] getArchivos() {
-        return archivos;
+    public DefaultListModel listarArchivos() {
+        if (archivo.exists()) {
+            archivos = new DefaultListModel();
+            for (File archivo : archivo.listFiles()) {
+                if (archivo.isFile() && archivo.isHidden() == false) {
+                    archivos.addElement(archivo.getName());
+                }
+            }
+            return archivos;
+        } else {
+            return null;
+        }
     }
 
-    public void setArchivos(File[] archivos) {
-        this.archivos = archivos;
+    public DefaultListModel listarDirectorios() {
+        if (archivo.exists()) {
+            archivos = new DefaultListModel();
+            for (File archivo : archivo.listFiles()) {
+                if (archivo.isDirectory() && archivo.isHidden() == false) {
+                    archivos.addElement(archivo.getName());
+                }
+            }
+            return archivos;
+        } else {
+            return null;
+        }
     }
-    
-    public List<String> listarArchivos() {
-        List archivos = new ArrayList();
-        return archivos;
+
+    public DefaultListModel listarArchivosOcultos() {
+        if (archivo.exists()) {
+            archivos = new DefaultListModel();
+            for (File archivo : archivo.listFiles()) {
+                if (archivo.isFile() && archivo.isHidden()) {
+                    archivos.addElement(archivo.getName());
+                }
+            }
+            return archivos;
+        } else {
+            return null;
+        }
     }
-    
-    public List<String> listarDirectorios() {
-        List directorios = new ArrayList();
-        return directorios;
-    } 
-    
-    public List<String> listarArchivosOcultos() {
-        List archivosOcultos = new ArrayList();
-        return archivosOcultos;
+
+    public DefaultListModel listarDirectoriosOcultos() {
+        if (archivo.exists()) {
+            archivos = new DefaultListModel();
+            for (File archivo : archivo.listFiles()) {
+                if (archivo.isDirectory() && archivo.isHidden()) {
+                    archivos.addElement(archivo.getName());
+                }
+            }
+            return archivos;
+        } else {
+            return null;
+        }
     }
-    
-    public List<String> listarDirectoriosOcultos() {
-        List directoriosOcultos = new ArrayList();
-        return directoriosOcultos;
+
+    public boolean crearDirectorio(String nombre) {
+        if (!ruta.substring(ruta.length()-1).equals("/")) {
+            ruta += "/";
+        }
+        File nuevo = new File(ruta + nombre);
+
+        if (nuevo.exists()) {
+            return false;
+            
+        } else {
+            nuevo.mkdir();
+            return true;
+            
+        }
     }
-    
-    public void crearDirectorio(String nombre) {
-        
-    }
-    
+
     public void eliminarDirectorio(String nombre) {
-        
+        File archivo = new File(nombre);
+        File[] archivos = archivo.listFiles();
+
+        if (archivos.length != 0) {
+            for (File elemento : archivos) {
+                if (elemento.isFile()) {
+                    elemento.delete();
+                } else if (elemento.isDirectory()) {
+                    eliminarDirectorio(elemento.getAbsolutePath());
+                }
+            }
+            archivo.delete();
+
+        } else {
+            archivo.delete();
+        }
+
     }
-    
-    public void renombrarDirectorio(String nombre) {
-        
+
+    public void renombrarDirectorio(String nombre, String nuevoNombre) {
+        File archivoSeleccionado = new File(nombre);
+        archivoSeleccionado.renameTo(new File(nuevoNombre));
+
     }
-    
+
     public String mostrarInformacion(String nombre) {
-        return null;
+        //se instancia un nuevo directorio con el seleccionado por el usuario
+        File archivo = new File(nombre);
+
+        //Obtenemos el AbsolutePath del directorio seleccionado
+        String pathAbsoluto = "       " + archivo.getAbsolutePath();
+
+        //Obtenemos el tamaño del directorio seleccionado
+        String tamañoArchivo = "\n\n       " + (archivo.length() / 1024) + " KBytes";
+
+        //Verificamos si el directorio seleccionado tiene permisos de lectura
+        String permisoLectura = "\n\n       ";
+        if (archivo.canRead()) {
+            permisoLectura += "Tiene permiso para lectura";
+        } else {
+            permisoLectura += "No tiene permiso para lectura";
+        }
+
+        //Verificamos si el directorio seleccionado tiene permisos de escritura
+        String permisoEscritura = "\n\n       ";
+        if (archivo.canWrite()) {
+            permisoEscritura += "Tiene permiso para escritura";
+        } else {
+            permisoEscritura += "No tiene permiso para escritura";
+        }
+
+        //Obtenemos la fecha de modificacion del directorio seleccionado
+        String fechaModificacion = "\n\n       " + new Date(archivo.lastModified());
+
+        //Agregamos los datos al jTextArea
+        return pathAbsoluto + tamañoArchivo + permisoLectura + permisoEscritura + fechaModificacion;
     }
 }
